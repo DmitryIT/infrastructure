eae5a66513fea808a612d311c627b9b814e92bd1
Ubuntu <ubuntu@ip-172-31-31-195.eu-west-1.compute.internal>
2017-10-28 14:58:57 +0000

    first commit

---
+++ install_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Install Docker
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get install     apt-transport-https     ca-certificates     curl     software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu    xenial    stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
+sudo groupadd docker
+sudo usermod -aG docker ubuntu
